34b1b96df5a61b115be0eaabad503b8d9ae2711f
picasso5536 <picasso5536@gmail.com>
2023-03-21 13:16:42 +0900

    add delivery vo

---
+++ src/main/java/com/animal/www/market/model/vo/DeliveryInfoVO.java
@@ -0,0 +1,45 @@
+package com.animal.www.market.model.vo;
+
+public class DeliveryInfoVO {
+	private String dinfo_idx, dinfo_name, dinfo_addr, mbr_nickname, dinfo_msg;
+
+	public String getDinfo_idx() {
+		return dinfo_idx;
+	}
+
+	public void setDinfo_idx(String dinfo_idx) {
+		this.dinfo_idx = dinfo_idx;
+	}
+
+	public String getDinfo_name() {
+		return dinfo_name;
+	}
+
+	public void setDinfo_name(String dinfo_name) {
+		this.dinfo_name = dinfo_name;
+	}
+
+	public String getDinfo_addr() {
+		return dinfo_addr;
+	}
+
+	public void setDinfo_addr(String dinfo_addr) {
+		this.dinfo_addr = dinfo_addr;
+	}
+
+	public String getMbr_nickname() {
+		return mbr_nickname;
+	}
+
+	public void setMbr_nickname(String mbr_nickname) {
+		this.mbr_nickname = mbr_nickname;
+	}
+
+	public String getDinfo_msg() {
+		return dinfo_msg;
+	}
+
+	public void setDinfo_msg(String dinfo_msg) {
+		this.dinfo_msg = dinfo_msg;
+	}
+}
---
+++ src/main/java/com/animal/www/market/model/vo/DeliveryStateVO.java
@@ -0,0 +1,21 @@
+package com.animal.www.market.model.vo;
+
+public class DeliveryStateVO {
+	private String dvr_idx, dvr_state;
+
+	public String getDvr_idx() {
+		return dvr_idx;
+	}
+
+	public void setDvr_idx(String dvr_idx) {
+		this.dvr_idx = dvr_idx;
+	}
+
+	public String getDvr_state() {
+		return dvr_state;
+	}
+
+	public void setDvr_state(String dvr_state) {
+		this.dvr_state = dvr_state;
+	}
+}
