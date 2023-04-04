5e5c3ed7601aa5dc5a4d18dd0a1ae0748c7e0ede
picasso5536 <picasso5536@gmail.com>
2023-04-04 16:28:12 +0900

    add PoliceVO

---
+++ src/main/java/com/animal/www/commons/vo/PoliceVO.java
@@ -0,0 +1,69 @@
+package com.animal.www.commons.vo;
+
+public class PoliceVO {
+	private String plc_idx, plc_info, plc_kate, plc_date, plc_nickname, plc_content, plc_state, mbr_nickname;
+
+	public String getMbr_nickname() {
+		return mbr_nickname;
+	}
+
+	public void setMbr_nickname(String mbr_nickname) {
+		this.mbr_nickname = mbr_nickname;
+	}
+
+	public String getPlc_idx() {
+		return plc_idx;
+	}
+
+	public void setPlc_idx(String plc_idx) {
+		this.plc_idx = plc_idx;
+	}
+
+	public String getPlc_info() {
+		return plc_info;
+	}
+
+	public void setPlc_info(String plc_info) {
+		this.plc_info = plc_info;
+	}
+
+	public String getPlc_kate() {
+		return plc_kate;
+	}
+
+	public void setPlc_kate(String plc_kate) {
+		this.plc_kate = plc_kate;
+	}
+
+	public String getPlc_date() {
+		return plc_date;
+	}
+
+	public void setPlc_date(String plc_date) {
+		this.plc_date = plc_date;
+	}
+
+	public String getPlc_nickname() {
+		return plc_nickname;
+	}
+
+	public void setPlc_nickname(String plc_nickname) {
+		this.plc_nickname = plc_nickname;
+	}
+
+	public String getPlc_content() {
+		return plc_content;
+	}
+
+	public void setPlc_content(String plc_content) {
+		this.plc_content = plc_content;
+	}
+
+	public String getPlc_state() {
+		return plc_state;
+	}
+
+	public void setPlc_state(String plc_state) {
+		this.plc_state = plc_state;
+	}
+}
